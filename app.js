44c03e0a95c5e99573f5f3b7589a9cab3fe4140d
keshavsoft <nknnkumar@yahoo.com>
2026-03-07 15:19:10 +0530

    ui and front end both perfect

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { router as routerFromV1 } from "./V1/routes.js";
 import { router as routerFromV2 } from "./V2/routes.js";
+import { router as routerFromV3 } from "./V3/routes.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,7 @@ var port = normalizePort(process.env.PORT || 3000);
 app.use(express.static('Public'));
 app.use("/V1", routerFromV1);
 app.use("/V2", routerFromV2);
+app.use("/V3", routerFromV3);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
